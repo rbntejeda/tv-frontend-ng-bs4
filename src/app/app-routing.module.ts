69e473c590ddcd02e174ccdc4758d7c7844e09c2
Ruben <rubentejedaroa@gmail.com>
2018-12-29 21:10:19 -0300

    add layout, routing and page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlaylistCreateComponent } from './playlist/playlist-create/playlist-create.component';
 import { PlaylistAdminComponent } from './playlist/playlist-admin/playlist-admin.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { LayoutMainComponent } from './page/layout-main/layout-main.component';
 
 const routes: Routes = [
   {
     path:"playlist",
+    component:LayoutMainComponent,
     children:[
       { path:"", component:PlaylistAdminComponent },
       { path:"create", component:PlaylistCreateComponent }
     ]
+  },
+  {
+    path:"**",
+    component:PageNotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaylistCreateComponent } from './playlist/playlist-create/playlist-create.component';
 import { PlaylistAdminComponent } from './playlist/playlist-admin/playlist-admin.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { NavbarComponent } from './page/navbar/navbar.component';
+import { LayoutMainComponent } from './page/layout-main/layout-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaylistCreateComponent,
-    PlaylistAdminComponent
+    PlaylistAdminComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    LayoutMainComponent
   ],
   imports: [
     BrowserModule,
